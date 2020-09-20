37f97fbdd2acb19f6a5f4bf531b1df671bc8de33
Robert Evans <raevans@miners.utep.edu>
2020-09-20 07:05:14 -0600

    added 2 methods and added comments

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,34 +6,33 @@
 
 int space_char(char c)
 {
-   if (c==' '||c=='\t'){
-     return 1;   
-   }
-    else{
+    if (c==' '||c=='\t'){//checks if space or tab
+        return 1;   
+    }
+    else {
         return 0;
     }
 }
 
 int non_space_char(char c)
 {
-    return !space_char(c);
+    return !space_char(c);//Pretty much the opposite of space_char
 }
 
-char *word_start(char *str)
+char *word_start(char *word)
 {
-    while (*str) ){
-        if (non_space_char(str)) return str;
-        str++;
+    while (*word) ){
+        if (non_space_char(word)) return str;//takes pointer to char at beginning of the word
+        word++;
     }
-    return str;
+    return word;
 }
 
 char *word_terminator(char *word)
 {
-    while(*word){
-         if(space_char(word)) return word;
+    while (*word){
+         if (space_char(word)) return word;//takes pointer to the space after the last char in word
          word++;
-        
     }
     return word;
 }
@@ -41,13 +40,24 @@ char *word_terminator(char *word)
 int count_words(char *str)
 {
     int count = 0;
-    char *pos = word_start(str);
-   
+    char *pos = word_start(str);//pointer is on the first char in the first word
+    while (*p){
+        count++;
+        pos=word_terminator(p);//pointer will go to first char in each word then the space behind the word til the end.
+        pos=word_start(p);
+    }
+    return count;
 }
 
 char *copy_str(char *inStr, short len)
 {
-    
+    char *copyStr = malloc((len+1) * sizeof(char);
+    int i = 0;
+    for (   ; i < len; i++){
+        copyStr[i] = inStr[i];
+    }
+    copyStr[len] = '\0';
+    return copyStr;
 }
 
 char **tokenize(char *str)
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -15,10 +15,11 @@ int non_space_char(char c);
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char *word_start(char *str); 
+char *word_start(char *word); 
 
 /* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+char
+*word_terminator(char *word);
 
 /* Counts the number of words in the string argument. */
 int count_words(char *str);
